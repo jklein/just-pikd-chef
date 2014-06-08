db7c1f03bd9e0c01802bca5fae9f6bdc43924f0a
Scott Sandler <scott.m.sandler@gmail.com>
2014-06-08 17:05:50 -0400

    use pg dump instead of schema commands for product data

--- cookbooks/jp_app/files/default/postgres_setup.php
+++ cookbooks/jp_app/files/default/postgres_setup.php
@@ -23,40 +23,21 @@ foreach ($databases as $database) {
     $connections[$database] = get_connection($database);
 }
 
-// Create types if they don't already exist
-maybe_create_type($connections['product'], 'measurement_unit', ['fl oz', 'oz', 'sq ft', 'lbs', 'count']);
-maybe_create_type($connections['product'], 'temperature_zone', ['frozen', 'cold', 'fresh', 'dry']);
+// load data only if DB is empty
+if (!has_results($connections['product'], "select 1 from information_schema.tables where table_schema = 'public';")) {
+    /* Some notes:
+    we should update this to use a pgpass file instead of the hard coded, plain text password ideally
 
-// Create tables
-// Note: need to make a manufacturer for any kind of supplier including a farmer, etc. who gives us produce
-$product_sql = <<<'Products'
-create table products (
-    product_id serial primary key,
-    name varchar(255) not null,
-    temperature_zone temperature_zone not null,
-    manufacturer_id int not null,
-    list_cost money not null,
-    category_id int not null,
-    description text,
-    units_per_case smallint,
-    measurement_unit measurement_unit,
-    measurement_value int,
-    upc_commodity int,
-    upc_vendor int,
-    upc_case int,
-    upc_item int,
-    source_warehouse_id int, -- should this live in another table?
-    vendor_name varchar(500),
-    length float, --what are the units?
-    width float,
-    height float,
-    cubic_volume float,
-    weight float,
-    shelf_life_days int
-);
-Products;
-maybe_create_table($connections['product'], 'products', $product_sql);
+    The dump is created with this command (via vagrant ssh):
+    pg_dump -h localhost -U postgres -d product -W -Fc  > product.dump
 
+    I found this didn't work when using the --clean option.
+    Also didn't use --create because I don't want DB permissions set
+    above to be lost.
+    */
+    putenv("PGPASSWORD=justpikd");
+    exec('/usr/bin/pg_restore -h localhost -U postgres -d product -Fc < /usr/share/nginx/html/data/product.dump');
+}
 
 $customer_sql = <<<'Customers'
 CREATE TABLE customers (
@@ -79,7 +60,7 @@ Customers;
 maybe_create_table($connections['customer'], 'customers', $customer_sql);
 
 
-//grant permissions, has to be done after tables are created
+//grant permissions, has to be done after tables are created and we just do this every time because why not
 $grant = "GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA public to jp_readwrite;";
 foreach ($databases as $database) {
     pg_query($connections[$database], $grant);
@@ -101,11 +82,11 @@ function has_results ($connection, $query) {
 
 function maybe_create_type($connection, $name, $values) {
     if (!has_results($connection, "select 1 from pg_type where typname = '" . $name . "';")) {
-      pg_query(
-          $connection,
-          "CREATE TYPE " . $name . " AS ENUM ('" . implode("','", $values) . "');"
-      );
-  }
+        pg_query(
+            $connection,
+            "CREATE TYPE " . $name . " AS ENUM ('" . implode("','", $values) . "');"
+        );
+    }
 }
 
 function maybe_create_table($connection, $table_name, $sql) {
