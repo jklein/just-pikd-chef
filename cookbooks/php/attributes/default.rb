b68cdb7dcf1eca85d5db1d70d738aea7c94f3ff1
Jonathan Klein <jonathan.n.klein@gmail.com>
2014-06-28 16:52:28 -0400

    Upgrading PHP to 5.5.14

--- cookbooks/php/attributes/default.rb
+++ cookbooks/php/attributes/default.rb
@@ -80,8 +80,8 @@ else
 end
 
 default['php']['url'] = 'http://us2.php.net/get'
-default['php']['version'] = '5.5.13'
-default['php']['checksum'] = '32d0fc26fccdb249a918c0e01ffb7b82' # This is the .tar.gz md5
+default['php']['version'] = '5.5.14'
+default['php']['checksum'] = 'b34262d4ccbb6bef8d2cf83840625201' # This is the .tar.gz md5
 default['php']['prefix_dir'] = '/usr/local'
 
 default['php']['configure_options'] = %W{--prefix=#{php['prefix_dir']}
