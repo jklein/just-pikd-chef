c086b9a12c51a76b2b8acff082e0a6b95334e3b6
Jonathan Klein <jonathan.n.klein@gmail.com>
2014-08-07 10:12:25 -0400

    Having chef start the monit service

--- cookbooks/monit/recipes/default.rb
+++ cookbooks/monit/recipes/default.rb
@@ -12,13 +12,24 @@ package 'monit' do
     action :install
 end
 
-
+# Get the global configuration
 cookbook_file "/etc/monit/monitrc" do
   source "monitrc"
   mode 0700
 end
 
+# Get our services config
 cookbook_file "/etc/monit/conf.d/justpikd" do
   source "justpikd-services.conf"
   mode 0700
+end
+
+# Start the monit service
+service "monit" do
+  supports restart: true, start: true, reload: true
+  action [:enable, :start]
+
+  start_command   "service monit start"
+  restart_command "service monit restart"
+  reload_command  "service monit reload"
 end
